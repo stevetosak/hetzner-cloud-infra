cb39ab41a576fd0e812e984023457949269f59b4
stefan toskovski <stefantoska84@gmail.com>
2026-01-11 20:45:06 +0100

    chore: clean up scripts

--- infra/stage/scripts/tf_taint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Mark all workers for recreation
-terraform taint 'hcloud_server.workers["k8s-wk1"]'
-terraform taint 'hcloud_server.workers["k8s-wk2"]'
-terraform taint 'hcloud_server.workers["k8s-wk3"]'
