44802f4c3f8e425dea8ecbd06a3662874e312c96
stefan toskovski <stefantoska84@gmail.com>
2026-06-07 14:05:46 +0200

    mosquitto auth/reachability, waste-bin-agent deployment

--- projects/wasteio/scripts/init_secrets.sh
+++ projects/wasteio/scripts/init_secrets.sh
@@ -2,4 +2,9 @@
 kubectl create secret generic credentials -n wasteio \
   --from-literal=DB_USER="" \
   --from-literal=DB_PASS="" \
-  --from-literal=JWT_SECRET=""
+  --from-literal=JWT_SECRET="" \
+  --from-literal=MQTT_PASSWORD=""
+
+kubectl create secret generic wasteio-bin-agent-credentials -n wasteio \
+  --from-literal=SIM_ADMIN_EMAIL="" \
+  --from-literal=SIM_ADMIN_PASSWORD=""
