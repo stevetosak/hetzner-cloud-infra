8c024e568c2dcc53a657157dc343863afc0deee1
stefan toskovski <stefantoska84@gmail.com>
2026-09-22 20:58:41 +0200

    authos: commit its four Secrets SOPS-encrypted, drop init_secrets.sh

--- projects/authos/scripts/init_secrets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl create secret generic db-credentials -n authos --from-env-file=/home/stevetosak/k8s/projects/authos/db-credentials.env
-
-kubectl create secret generic keystore -n authos --from-file=/etc/keystore/keystore.p12
-kubectl create secret generic keystore-pass -n authos --from-literal=KEYSTORE_PASS="<>"
